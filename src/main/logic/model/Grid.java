4c7f57db1fabf183541958be28e06c95b08c95ba
Gillan0 <antonino.gillard@orange.fr>
2024-12-22 21:29:55 +0100

    Commented Grid.java

--- src/main/logic/model/Grid.java
+++ src/main/logic/model/Grid.java
@@ -3,24 +3,53 @@ package main.logic.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *  The Grid class represents a 2d grid.
+ */
 public class Grid {
 
+	/**
+	 * Content of the grid
+	 */
 	protected Integer[][] grid; 
+	
+	/**
+	 * Number of lines of the grid
+	 */
 	protected int height;
+	
+	/**
+	 * Number of columns of the grid
+	 */
 	protected int width;
 	
+	/**
+	 * Constructor to create a Grid. Default is a 25 pixel side long square.
+	 */
 	public Grid() {
 		this(25);
 	}
 	
+	/**
+	 * Constructor to create a square Grid of side s.
+	 * 
+	 * @param s Number of pixels on the side
+	 */
 	public Grid(int s) {
 		this(s, s);
 	}
-
+	
+	/**
+	 * Constructor to create a rectangular Grid.
+	 * 
+	 * @param height Index of pixels high
+	 * @param width  Index of pixels long
+	 */
 	public Grid(int height, int width) {
 		this.height = height;
 		this.width = width;
 		
+		// Fill the grid with 0
 		grid = new Integer[height][width];
 		for (int i = 0 ; i < height ; i++) {
 			for (int j = 0; j < width ; j++) {
@@ -29,13 +58,30 @@ public class Grid {
 		}
 	}
 
+	/**
+	 * Gets the element of the grid on line i and column j
+	 * 
+	 * @param i Index of the chosen line
+	 * @param j Index of the chosen column
+	 * @return Element at line i and column j
+	 * @throws GridHeightException Raised when line i not in table
+	 * @throws GridWidthException Raised when column j not in table
+	 */
 	public Integer getElement(int i, int j) throws GridHeightException, GridWidthException {
 		checkGridHeightException(i);
 		checkGridWidthException(j);
-		
 		return grid[i][j];
 	}
 	
+	/**
+	 * Sets a new element in the grid on line i and column j
+	 * 
+	 * @param i Index of the chosen line
+	 * @param j Index of the chosen column
+	 * @param value New element at line i and column j
+	 * @throws GridHeightException Raised when line i not in table
+	 * @throws GridWidthException Raised when column j not in table
+	 */
 	public void setElement(int i, int j, int value) throws GridHeightException, GridWidthException {
 		checkGridHeightException(i);
 		checkGridWidthException(j);
@@ -43,34 +89,29 @@ public class Grid {
 		grid[i][j] = value;
 	}
 	
-	public void setLine(int i, ArrayList<Integer> value) throws GridHeightException {
-		checkGridHeightException(i);
-		
-		for (int j = 0; j < width; j++) {
-			grid[i][j] = value.get(j);
-		}
-	}
-	
-	public void setColumn(int j, ArrayList<Integer> value) throws GridWidthException {
-		checkGridWidthException(j);
-		
-		for (int i = 0; i < height; i++) {
-	        grid[i][j] = value.get(i);
-	    }
-	}
-	
+	/**
+	 * Get the number of lines of the grid
+	 * @return Total number of lines of the grid
+	 */
 	public int getHeight() {
 		return height;
 	}
 	
+	/**
+	 * Get the number of columns of the grid
+	 * @return Total number of columns of the grid
+	 */
 	public int getWidth() {
 		return width;
 	}
 	
-	public String getGrid() {
-		return grid.toString();
-	}
-	
+	/**
+	 * Gets the content of line n
+	 * 
+	 * @param n Index of the chosen line
+	 * @throws GridHeightException Raised when line i not in table
+	 * @return Line number n
+	 */
  	public ArrayList<Integer> getLine(int n) throws GridHeightException {
 		checkGridHeightException(n);
 		ArrayList<Integer> line = new ArrayList<Integer>();
@@ -80,6 +121,13 @@ public class Grid {
 		return line;
 	}
 	
+ 	/**
+	 * Gets the content of column n
+	 * 
+	 * @param n Index of the chosen column
+	 * @throws GridWidthException Raised when column j not in table
+	 * @return Column number n
+	 */
 	public ArrayList<Integer> getColumn(int n) throws GridWidthException {
 		checkGridWidthException(n);
 		ArrayList<Integer> line = new ArrayList<Integer>();
@@ -90,48 +138,86 @@ public class Grid {
 	}
 	
 	/**
-	 * WIP - NOT FINISHED
+	 * Sets the content of line i
+	 * 
+	 * @param i Index of the chosen line
+	 * @param value List of number to be put in the line
+	 * @throws GridHeightException Raised when line i not in table
+	 */
+	public void setLine(int i, ArrayList<Integer> value) throws GridHeightException {
+		checkGridHeightException(i);
+		
+		// Fill line
+		for (int j = 0; j < width; j++) {
+			grid[i][j] = value.get(j);
+		}
+	}
+	
+	/**
+	 * Sets the content of column j
+	 * 
+	 * @param j Index of the chosen column
+	 * @param value List of number to be put in the column
+	 * @throws GridWidthException Raised when column j not in table
+	 */
+	public void setColumn(int j, ArrayList<Integer> value) throws GridWidthException {
+		checkGridWidthException(j);
+		
+		for (int i = 0; i < height; i++) {
+	        grid[i][j] = value.get(i);
+	    }
+	}
+	
+	/**
+	 * Gives a visual representation of the grid
+	 * @return String representing the grid
 	 */
 	public String toString() {
 		StringBuilder description = new StringBuilder();
 	    
-	    // Iterate through each row of the grid
 	    for (int i = 0; i < height; i++) {
-	        // Iterate through each column in the row
 	        for (int j = 0; j < width; j++) {
 	            try {
 	                int element = this.getElement(i, j);
 	                
-	                // Conditionally add the correct string representation for -1, 0, and 1
 	                if (element == -1) {
-	                    description.append("_ ");  // or any other character for -1
+	                    description.append("_ ");  
 	                } else if (element == 0) {
-	                    description.append("O ");  // or any other character for 0
+	                    description.append("O ");  
 	                } else if (element == 1) {
-	                    description.append("1 ");  // or any other character for 1
+	                    description.append("1 ");  
 	                } else {
-	                    description.append(element).append(" ");  // Default for other values
+	                    description.append(element).append(" "); 
 	                }
 	            } catch (GridHeightException | GridWidthException e) {
-	                // Handle exceptions (although this shouldn't occur in this context)
+	                // Shouldn't occur
 	                e.printStackTrace();
 	            }
 	        }
-	        // Add a newline after each row for better formatting
 	        description.append("\n");
 	    }
 	    return description.toString();
 	}
 	
+	/**
+	 * Checks if this object instance is equal to another object
+	 * 
+	 * @param o Another object
+	 * @return boolean if this is equal to o
+	 */
 	public boolean equals(Object o) {
+		// Compare type
 		if (!(o instanceof Grid)) {
 			return false;
 		}
+		
+		// Compare Grid dimensions
 		Grid h = (Grid) o;
 		if (height != h.getHeight() || width != h.getWidth()) {
 			return false;
 		}
 		
+		// Compare Grid content
 		for (int i = 0; i < height ; i++) {
 			for (int j = 0; j < width ; j++) {
 				try {
@@ -147,12 +233,24 @@ public class Grid {
 		return true;
 	}
 		
+	/**
+	 * Checks if the chosen line index is in table
+	 * 
+	 * @param n Index of chosen line
+	 * @throws GridHeightException
+	 */
 	protected void checkGridHeightException(int n) throws GridHeightException {
 		if (n >= height) {
 			throw new GridHeightException("Line Index out of bounds");
 		}
 	}
 	
+	/**
+	 * Checks if the chosen column index is in table
+	 * 
+	 * @param n Index of chosen column
+	 * @throws GridWidthException
+	 */
 	protected void checkGridWidthException(int n) throws GridWidthException {
 		if (n >= width) {
 			throw new GridWidthException("Column Index out of bounds");
